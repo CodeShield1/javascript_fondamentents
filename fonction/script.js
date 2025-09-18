cee435da9018f66b210b5c1457306c4a6d3adfbb
brahim_medkour <medkourbrahim08@.com>
2025-09-18 15:03:20 +0100

    le codde de fonction

---
+++ fonction/script.js
@@ -0,0 +1,19 @@
+function names() {
+    console.log("brahim");
+}
+
+names();
+
+
+function saluer(nom) {
+  console.log("Bonjour " + nom + " !");
+}
+saluer("Ali");
+
+
+
+function addition(a, b) {
+  return a + b;
+}
+let resultat = addition(5, 3);
+console.log("la somme " +resultat); // Affiche 8
