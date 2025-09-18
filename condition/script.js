6e260763362592705c8f500036efd844e0cb862d
brahim_medkour <medkourbrahim08@gmail.com>
2025-09-18 12:08:06 +0100

    ajouter code  de condition

---
+++ condition/script.js
@@ -0,0 +1,28 @@
+let age = 18;
+if (age >= 18) {
+    console.log("Vous êtes majeur.");
+} else {
+    console.log("Vous êtes mineur.");
+}   
+
+
+
+let jeure = "lundi";
+switch (jeure) {
+    case "lundi":
+        console.log("Aujourd'hui, c'est lundi.");
+        break;
+    case "mardi":
+        console.log("Aujourd'hui, c'est mardi.");
+        break;
+}
+
+
+let ages = 20;
+
+if (ages < 18) {
+  console.log("Vous êtes mineur.");
+} else if (ages === 18) {
+  console.log("Vous venez d’atteindre la majorité !");
+} else {
+  console.log("Vous êtes majeur.");}
