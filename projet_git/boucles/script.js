45e6b38071a9f8b3f462a56d1c2dc9f249dbad21
brahim_medkour <medkourbrahim08@.com>
2025-09-19 09:52:53 +0100

    crée fichier js

---
+++ projet_git/boucles/script.js
@@ -0,0 +1,23 @@
+for(i=1;i<=10;i++){
+    
+    console.log(i);
+
+}
+
+
+ for(h=1;h<=10;h++){
+    if(h===5){
+        continue;
+
+    }else{
+        console.log(h);
+    }
+    
+
+ }
+
+
+ 
+
+
+
